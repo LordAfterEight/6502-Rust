a4fb5902aa6eca0fa2abb10156aad2fb8a0e9423
LordAfterEight <elias@stettmayer.de>
2025-05-08 16:36:44 +0200

    Added missing eventhandler.rs

---
+++ src/eventhandler.rs
@@ -0,0 +1,16 @@
+use crate::crossterm::event::{self, KeyCode, KeyEventKind};
+
+pub fn read_event() -> event::KeyCode {
+    loop{
+        let mut key_code = event::KeyCode::Char(' ');
+        if let Ok(event::Event::Key(key)) = event::read() {
+            if key.kind == event::KeyEventKind::Press {
+                key_code = key.code;
+            }
+        }
+        if key_code == event::KeyCode::Enter {
+            continue;
+        }
+        return key_code;
+    }
+}
